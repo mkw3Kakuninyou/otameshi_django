a4747fb792fa8e5a3b0fccffe47bd12149871e92
mkw3Kakuninyou <mkw3officialmail@gmail.com>
2023-07-16 13:52:13 +0900

    カテゴリー改良20230716

--- myapp/models.py
+++ myapp/models.py
@@ -3,7 +3,7 @@ from django.contrib.auth.models import User
 
 
 class Category(models.Model):
-    name = models.CharField('カテゴリ名', max_length=50)
+    name = models.CharField('カテゴリ名', max_length=50, unique=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
     
